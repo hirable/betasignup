0720a74dc6dd5591189dc16a0d97bd16e9f71f86
Evelyn Hu <evelyn.x.hu@gmail.com>
2013-06-14 13:02:39 -0700

    Embedded animation video

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Landingpage::Application.config.secret_token = 'ed6efee5e12836f0279620742963dace2134e650d54a7697b0366933f2fca080dc6f0413ad58a3d88b96b9b857f3f41f6b925405688d9cf5cd672c24fe06ecf9'
