eee1da79bf822516a05d2de645c04656a474685d
Evelyn Hu <evelyn.x.hu@gmail.com>
2013-07-15 22:58:25 -0700

    validating and success and error messages

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -6,15 +6,16 @@ class SignupsController < ApplicationController
   def create
     @signup = Signup.new(params[:signup])
     if @signup.save
-      flash[:failure] = ""
+      flash[:failure] = nil
       flash[:success] = "Thanks! Got it. We'll notify you soon!"
       redirect_to root_path
     else
-      flash[:success] =""
+      flash[:success] = nil
       flash[:failure] = ""
       @signup.errors.full_messages.each do |msg|
         flash[:failure] = flash[:failure] + msg + ". "
       end
+      flash[:failure] = flash[:failure] + "Try Again!"
       render 'new'
     end
   end
